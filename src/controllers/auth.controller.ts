87e1a9f65548e8663193fc8554061e401e30495b
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-11-14 11:07:39 +0700

    feat: authentication feature

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { AuthService } from "../services/auth.service";
+
+export class AuthController {
+  private authService: AuthService;
+
+  constructor() {
+    this.authService = new AuthService();
+  }
+
+  async login(req: Request, res: Response): Promise<void> {
+    const { email, password } = req.body;
+
+    try {
+      const token = await this.authService.login(email, password);
+      res.status(200).send({
+        message: "Successfully logged in",
+        status: res.statusCode,
+        access_token: token,
+      });
+    } catch (error: any) {
+      res.status(404).send({
+        message: "Failed to logged in. Check your email or password",
+        status: res.statusCode,
+      });
+    }
+  }
+}
---
+++ src/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+export class AuthService {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+  async login(email: string, password: string): Promise<string | null> {
+    const employee = await this.prisma.employee.findUnique({
+      where: { email: email, password: password },
+    });
+
+    if (!employee) {
+      throw new Error("Employee not found");
+    }
+
+    const token = jwt.sign(
+      { id: employee.id, email: employee.email, role: employee.role },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    return token;
+  }
+}
