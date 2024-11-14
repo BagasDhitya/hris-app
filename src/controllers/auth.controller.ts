3b9c82df7e08a96742267b2ab1d04b2d7169c633
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-11-14 15:22:43 +0700

    chore: add debugger for tracing /api/auth/login issue

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -13,16 +13,23 @@ export class AuthController {
 
     try {
       const token = await this.authService.login(email, password);
+      console.log("token : ", token);
       res.status(200).send({
         message: "Successfully logged in",
         status: res.statusCode,
         access_token: token,
       });
-    } catch (error: any) {
+    } catch (error) {
+      const errorDetails = {
+        name: error instanceof Error ? error.name : "UnknownError",
+        detail:
+          error instanceof Error ? error.message : "An unknown error occurred",
+      };
+      console.log("track error : ", errorDetails);
       res.status(404).send({
-        message: "Failed to logged in. Check your email or password",
+        message: "Failed to log in. Check your email or password",
         status: res.statusCode,
-        details: error,
+        details: errorDetails,
       });
     }
   }
