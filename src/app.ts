8bd15bd5b4ae120f84c9129c2fabcca9e06a994c
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2024-11-14 20:48:16 +0700

    fix: cors config

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,19 @@ import express from "express";
 import employeeRouter from "./routers/employee.router";
 import hrRouter from "./routers/hr.router";
 import authRouter from "./routers/auth.router";
+import cors from "cors";
 
 import { ErrorHandler } from "./middlewares/error.middleware";
 
 const app = express();
 const errorHandler = new ErrorHandler();
 const port = 8000;
+
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+  })
+);
 
 app.use(express.json());
 app.use("/api/employee", employeeRouter);
